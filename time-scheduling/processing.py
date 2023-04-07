0f20857d1c6fedcf65fecaf9c7e44ca07b35454f
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-07 16:19:36 +0700

    .

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -76,7 +76,7 @@ def load_file():
     
     col1, col2 = st.columns(2)
     with col1:
-        st.experimental_data_editor(df1[['course_name', 'Lab', 'ToTH_Lab', 'size', 'duration', 'prof_id', 'prof_name']], num_rows="dynamic")
+        df1 = st.experimental_data_editor(df1[['course_name', 'Lab', 'ToTH_Lab', 'size', 'duration', 'prof_id', 'prof_name']], num_rows="dynamic")
 
     with col2:
         with st.expander("Edit your data"):
