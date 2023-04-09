362592efa9f15381a2ab43e31518cf08fcc60e47
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-09 22:57:26 +0700

    .

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -66,7 +66,7 @@ def load_file():
     with col1:
         df1 = st.experimental_data_editor(df1, num_rows="dynamic")
         df1['group_id'] = np.arange(1, len(df1) + 1)
-        st.write("df1 :" + type(df1))
+        st.write(type(df1))
     with col2:
         df_room = st.experimental_data_editor(df_room, num_rows="dynamic")
         df_room['size'] = df_room['size'].astype(int)
