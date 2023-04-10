d6a229a32ab621bf062a3f03af5e7b0b8992e7ac
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-10 09:57:52 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -67,6 +67,26 @@ def load_file():
         df2 = st.experimental_data_editor(df1, num_rows="dynamic")
         if st.button('Save data'):
             df2['group_id'] = np.arange(1, len(df2) + 1)
+            list_course = []
+            index_count_course_id = 0
+            list_prof = []     
+            index_count_prof_id = 0
+
+            for index1, row1 in df2.iterrows():
+                if row1['course_name'] not in list_course:
+                    df2.at[index1, 'course_id'] = index_count_course_id + 1
+                    index_count_course_id += 1
+                    list_course.append(row1['course_name'])
+                else:
+                    df2.at[index1, 'course_id'] = index_count_course_id
+
+                if row1['prof_name'] not in list_prof:
+                    df2.at[index1, 'prof_id'] = index_count_prof_id + 1
+                    index_count_prof_id += 1
+                    list_prof.append(row1['prof_name'])
+                else:
+                    df2.at[index1, 'prof_id'] = index_count_prof_id
+            
             st.write(df2)
 
     with col2:
