d5f19e486cc3d50d4bf5c200d8fa3214efd51a91
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-16 11:06:15 +0700

    z

--- time-scheduling/HtmlOutput.py
+++ time-scheduling/HtmlOutput.py
@@ -115,11 +115,11 @@ class HtmlOutput:
         sb = []
         if rowspan > 1:
             sb.append("<td style='border: .1em solid black; padding: .25em' rowspan='")
-            sb.append(rowspan)
+            # sb.append(rowspan)
             sb.append("'>")
         else:
             sb.append("<td style='border: .1em solid black; padding: .25em' rowspan='")
-            sb.append(rowspan)
+            # sb.append(rowspan)
             sb.append("'>")
         sb.append(content)
         sb.append("</td>")
