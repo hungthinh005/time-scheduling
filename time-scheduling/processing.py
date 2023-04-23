7215cf9d5b15bfc81ca6642c1806a3ebbadcd6ec
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-23 14:37:17 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -160,6 +160,19 @@ def load_file():
     with open('GaSchedule1.json', 'w') as f:
         f.write(json_data) 
 
+def for_stu():
+    df_stu = pd.read_csv('data_stu.csv')
+    df_stu = df_stu[['MaSV', 'NHHK', 'HK', 'MaMH', 'TenMH', 'SoTinChi', 'DiemHP']]
+    df_stu = df_stu.dropna()
+    df_stu['NHHK'] = df_stu['NHHK'].astype(str).str[:-1]
+
+
+    text_search = st.text_input("Search...")
+    m1 = df_stu["MaSV"].str.contains(text_search, case = False)
+    m2 = df_stu["TenMH"].str.contains(text_search, case = False)
+    df_search = df_stu(m1|m2)
+
+
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
@@ -176,3 +189,4 @@ if __name__ == "__main__":
             traceback.print_exc()
     with tab2:
         st.header("Student Recommend")
+        for_stu()
