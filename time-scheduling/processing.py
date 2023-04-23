87883868b204b2ffaadd61e936a43a3ce83827e3
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-23 15:21:53 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -182,7 +182,8 @@ def for_stu():
             </style>
             """
             st.markdown(hide_table_row_index, unsafe_allow_html=True)
-            st.table(list_subject_have_done)
+            with st.expander("List of subjects have done"):  
+                st.table(list_subject_have_done)
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
