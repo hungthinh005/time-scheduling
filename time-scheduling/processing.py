118d9a97d4b9557eaf979e814889a35e0d12b867
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-25 10:12:01 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -187,13 +187,13 @@ def for_stu():
             """
             st.markdown(hide_table_row_index, unsafe_allow_html=True)
             with st.expander("List of subjects have done"):  
-                st.table(list_subject_have_done)
+                st.dataframe(list_subject_have_done)
     with col2:
         if input:
-            list_subject_havent_done_yet = df_ctdt[~df_ctdt['Course Name'].isin(list_subject_have_done['Course Name'])]
+            list_subject_havent_done_yet = df_ctdt[~df_ctdt['MaMH'].isin(list_subject_have_done['MaMH'])]
             with st.expander("List of subjects haven't done yet"):  
                 st.markdown(hide_table_row_index, unsafe_allow_html=True)
-                st.table(list_subject_havent_done_yet)
+                st.dataframe(list_subject_havent_done_yet)
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
