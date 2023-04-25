d63b5c56bbe16fcdc311745943b8346495b43145
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-25 10:38:01 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -186,11 +186,11 @@ def for_stu():
             #                         </style>
             #                         """
             
-            # with st.expander("List of subjects have done"):  
+            with st.expander("List of subjects have done"):  
             # st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-            list_subject_have_done = list_subject_have_done.style.hide_index()
-            st.dataframe(list_subject_have_done.to_html(), unsafe_allow_html=True)
-            # st.dataframe(list_subject_have_done)
+            # list_subject_have_done = list_subject_have_done.style.hide_index()
+            # st.dataframe(list_subject_have_done.to_html(), unsafe_allow_html=True)
+                st.dataframe(list_subject_have_done.assign(hack='').set_index(''))
     with col2:
         if input:
             list_subject_havent_done_yet = df_ctdt[~df_ctdt['MaMH'].isin(list_subject_have_done['MaMH'])]
@@ -204,9 +204,9 @@ def for_stu():
                 # </style>
                 # """
                 # st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
-                # st.dataframe(list_subject_havent_done_yet)
-                list_subject_havent_done_yet = list_subject_havent_done_yet.style.hide_index()
-                st.dataframe(list_subject_havent_done_yet.to_html(), unsafe_allow_html=True)
+                st.dataframe(list_subject_havent_done_yet.assign(hack='').set_index(''))
+                # list_subject_havent_done_yet = list_subject_havent_done_yet.style.hide_index()
+                # st.dataframe(list_subject_havent_done_yet.to_html(), unsafe_allow_html=True)
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
