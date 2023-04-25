6a8c12b64732522e6dfe32d4c13a390b83d40ca4
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-25 09:40:07 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -163,7 +163,7 @@ def load_file():
 def for_stu():
     df_stu = pd.read_csv("time-scheduling/data_stu.csv")
     df_ctdt = pd.read_csv("time-scheduling/ctdt_ds.csv")
-    df_ctdt = df_ctdt[['Course Name', 'Sem', 'Year', 'Credits']]
+    df_ctdt = df_ctdt[['MaMH', 'Course Name', 'Sem', 'Year', 'Credits']]
     df_stu = df_stu[['MaSV', 'NHHK', 'HK', 'MaMH', 'TenMH', 'SoTinChi', 'DiemHP']]
     df_stu = df_stu.dropna()
     df_stu['NHHK'] = df_stu['NHHK'].astype(str).str[:-1]
@@ -173,9 +173,9 @@ def for_stu():
     with col1:
         if input:
             df_stu = df_stu.loc[df_stu['MaSV'].str.lower() == input.lower()]
-            list_subject_have_done = df_stu[['TenMH','HK', 'NHHK', 'SoTinChi']]
+            list_subject_have_done = df_stu[['MaMH', 'TenMH','HK', 'NHHK', 'SoTinChi']]
             list_subject_have_done[''] = np.arange(1, len(list_subject_have_done) + 1) 
-            list_subject_have_done = list_subject_have_done.reindex(columns=['', 'TenMH','HK', 'NHHK', 'SoTinChi'])
+            list_subject_have_done = list_subject_have_done.reindex(columns=['', 'MaMH', 'TenMH','HK', 'NHHK', 'SoTinChi'])
             list_subject_have_done = list_subject_have_done.rename(columns={'NHHK': 'Year', 'HK': 'Sem', 'TenMH': 'Course Name', 'SoTinChi': 'Credits'}) 
             hide_table_row_index = """
             <style>
@@ -185,13 +185,13 @@ def for_stu():
             """
             st.markdown(hide_table_row_index, unsafe_allow_html=True)
             with st.expander("List of subjects have done"):  
-                st.table(list_subject_have_done)
+                st.dataframe(list_subject_have_done)
     with col2:
         if input:
-            list_subject_havent_done_yet = df_ctdt[~df_ctdt['Course Name'].isin(list_subject_have_done['Course Name'])]
+            list_subject_havent_done_yet = df_ctdt[~df_ctdt['MaMH'].isin(list_subject_have_done['MaMH'])]
             with st.expander("List of subjects haven't done yet"):  
                 st.markdown(hide_table_row_index, unsafe_allow_html=True)
-                st.table(list_subject_havent_done_yet)
+                st.dataframe(list_subject_havent_done_yet)
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
