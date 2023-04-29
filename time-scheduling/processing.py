db20252f3cd9333fe73f2524d5d32105b9f12bf2
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-29 23:43:13 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -209,10 +209,10 @@ def for_stu():
     with col3:
         df_unique = df2.dropna()
         df_unique = df_unique[df_unique['Group_Lab'] == 1.0]
-        df_unique = df_unique[['MaMH', 'Course Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course']]
+        df_unique = df_unique[['MaMH', 'Course_Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course']]
         list_recommend_subjects = df_unique[df_unique['MaMH'].isin(list_subject_havent_done_yet['MaMH'])]
         list_recommend_subjects[''] = np.arange(1, len(list_recommend_subjects) + 1) 
-        list_recommend_subjects = list_recommend_subjects.reindex(columns=['', 'MaMH', 'Course Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course'])
+        list_recommend_subjects = list_recommend_subjects.reindex(columns=['', 'MaMH', 'Course_Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course'])
         with st.expander("List of recommend subjects in this semester"):    
             st.dataframe(list_recommend_subjects.set_index(''))  
 
