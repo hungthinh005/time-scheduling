9c525b878c71f997094858df98a934051ce5bfc0
hungthinh005 <phanhungthinh555@gmail.com>
2023-04-29 23:06:30 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -15,65 +15,8 @@ import traceback
 
 ##load file and processing data
 
-def data():    
-
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-
-    else:
-        df = [['Data Mining', 1, 35, 4, 'Nguyen Thi Thanh Sang', 'ITDS'],
-            ['AOD', 2, 35, 4, 'Nguyen Thi Thanh Sang', 'ITDS'],
-            ['Functional Programming', 0, 90, 3, 'Dao Tran Hoang Chau', 'ITDS'],
-            ['Operating Systems', 0, 90, 3, 'Tran Manh Ha', 'ITDS']]
-        room_columns = ['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']
-        df = pd.DataFrame(df, columns=room_columns)
+ 
     
-    df1 = df[['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']]
-    df1 = df1.rename(columns={'TenMH': 'Course_Name', 'ToTH': 'Group_Lab', 'TongSoSV': 'Size_Course', 'SoTiet': 'Duration', 'TenDayDuNV': 'Prof_Name', 'MaMH': 'MaMH'})
-    df1['Lab'] = df1['Group_Lab']
-    # df1['Lab'] = df1['Lab'].astype(str)
-    for index, row in df1.iterrows():
-        if row['Lab'] == 1 or row['Lab'] == 2 or row['Lab'] == 3 or row['Lab'] == 4:
-            df1.at[index, 'Lab'] = 'True'
-        else:
-            df1.at[index, 'Lab'] = ''
-    df1['Lab'] = df1['Lab'].astype(bool)
-    
-
-
-    ## create default room
-    room_default = [['A1.309', 90, 0],
-                    ['L107', 40, 0],
-                    ['LA1.605', 35, 1],
-                    ['La1.607', 35, 1]
-    ]
-    room_columns = ['Room', 'Size_Room', 'Lab']
-    df_room = pd.DataFrame(room_default, columns=room_columns)
-    df_room['Lab'] = df_room['Lab'].astype(str)
-    for index, row in df_room.iterrows():
-        if row['Lab'] == '1':
-            df_room.at[index, 'Lab'] = 'True'
-        else:
-            df_room.at[index, 'Lab'] = ''
-    df_room['Lab'] = df_room['Lab'].astype(bool)
-
-    col1, col2, col3 = st.columns([7,2.4,4.5])
-    with col1:
-        df2 = st.experimental_data_editor(df1, num_rows="dynamic")
-        df2['Group_id'] = np.arange(1, len(df2) + 1)
-
-    with col2:
-        df_room = st.experimental_data_editor(df_room, num_rows="dynamic")
-        df_room['Size_Room'] = df_room['Size_Room'].astype(int)
-
-        
-    with col3:
-        with st.expander("Descriptions for Data Input"):    
-            st.write("- Must Include: Course Name, Lab Group, Size of Course, Duration (Period of Course), Professor Name.")
-            st.write("- In the case of the course with 4 periods, 1 room can only accommodate 12 classes at most. Be careful when modifying the info on rooms")
-    
-
-    return df2, df_room
 
 
 def load_file():
@@ -135,7 +78,6 @@ def load_file():
     #         st.write("- Must Include: Course Name, Lab Group, Size of Course, Duration (Period of Course), Professor Name.")
     #         st.write("- In the case of the course with 4 periods, 1 room can only accommodate 12 classes at most. Be careful when modifying the info on rooms")
     # df2['Group_id'] = np.arange(1, len(df2) + 1)   
-    df2, df_room = data()
     
     list_course = []
     index_count_course_id = 0
@@ -228,7 +170,7 @@ def load_file():
 
 
 
-def for_stu():
+def for_stu(df2):
     df_stu = pd.read_csv("time-scheduling/data_stu.csv")
     df_ctdt = pd.read_csv("time-scheduling/ctdt_ds.csv")
 
@@ -266,8 +208,7 @@ def for_stu():
 
     with col3:
         if input:
-            df_unique, df_room = data()
-            df_unique['MaMH'] = df_unique['MaMH'].drop_duplicates()
+            df_unique['MaMH'] = df2['MaMH'].drop_duplicates()
             df_unique = df_unique[['MaMH', 'Course Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course']]
             df_unique = pd.DataFrame(df_unique)
             list_recommend_subjects = df_unique[df_unique['MaMH'].isin(list_subject_havent_done_yet['MaMH'])]
@@ -278,9 +219,65 @@ def for_stu():
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
+
+    uploaded_file = st.file_uploader('')
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+
+    else:
+        df = [['Data Mining', 1, 35, 4, 'Nguyen Thi Thanh Sang', 'ITDS'],
+            ['AOD', 2, 35, 4, 'Nguyen Thi Thanh Sang', 'ITDS'],
+            ['Functional Programming', 0, 90, 3, 'Dao Tran Hoang Chau', 'ITDS'],
+            ['Operating Systems', 0, 90, 3, 'Tran Manh Ha', 'ITDS']]
+        room_columns = ['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']
+        df = pd.DataFrame(df, columns=room_columns)
+
+    df1 = df[['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']]
+    df1 = df1.rename(columns={'TenMH': 'Course_Name', 'ToTH': 'Group_Lab', 'TongSoSV': 'Size_Course', 'SoTiet': 'Duration', 'TenDayDuNV': 'Prof_Name', 'MaMH': 'MaMH'})
+    df1['Lab'] = df1['Group_Lab']
+    # df1['Lab'] = df1['Lab'].astype(str)
+    for index, row in df1.iterrows():
+        if row['Lab'] == 1 or row['Lab'] == 2 or row['Lab'] == 3 or row['Lab'] == 4:
+            df1.at[index, 'Lab'] = 'True'
+        else:
+            df1.at[index, 'Lab'] = ''
+    df1['Lab'] = df1['Lab'].astype(bool)
+    
+
+    ## create default room
+    room_default = [['A1.309', 90, 0],
+                    ['L107', 40, 0],
+                    ['LA1.605', 35, 1],
+                    ['La1.607', 35, 1]
+    ]
+    room_columns = ['Room', 'Size_Room', 'Lab']
+    df_room = pd.DataFrame(room_default, columns=room_columns)
+    df_room['Lab'] = df_room['Lab'].astype(str)
+    for index, row in df_room.iterrows():
+        if row['Lab'] == '1':
+            df_room.at[index, 'Lab'] = 'True'
+        else:
+            df_room.at[index, 'Lab'] = ''
+    df_room['Lab'] = df_room['Lab'].astype(bool)
+
+    col1, col2, col3 = st.columns([7,2.4,4.5])
+    with col1:
+        df2 = st.experimental_data_editor(df1, num_rows="dynamic")
+        df2['Group_id'] = np.arange(1, len(df2) + 1)
+
+    with col2:
+        df_room = st.experimental_data_editor(df_room, num_rows="dynamic")
+        df_room['Size_Room'] = df_room['Size_Room'].astype(int)
+
+        
+    with col3:
+        with st.expander("Descriptions for Data Input"):    
+            st.write("- Must Include: Course Name, Lab Group, Size of Course, Duration (Period of Course), Professor Name.")
+            st.write("- In the case of the course with 4 periods, 1 room can only accommodate 12 classes at most. Be careful when modifying the info on rooms")
+    
+
     tab1, tab2 = st.tabs(["Schedule", "Student"])
     with tab1:
-        uploaded_file = st.file_uploader('')
         load_file()
         file_name = "/GaSchedule1.json"
         if len(sys.argv) > 1:
@@ -291,5 +288,5 @@ if __name__ == "__main__":
         except:
             traceback.print_exc()
     with tab2:
-        for_stu()
+        for_stu(df2)
 
