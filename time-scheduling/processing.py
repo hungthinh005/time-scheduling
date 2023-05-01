9c5659ac8f434f47c5a6007e97bd646a9b53e1d1
hungthinh005 <phanhungthinh555@gmail.com>
2023-05-01 13:40:10 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -215,7 +215,7 @@ def for_stu():
             df_unique = df_unique[['MaMH', 'Course_Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course']]
             list_recommend_subjects = list_subject_havent_done_yet[list_subject_havent_done_yet['MaMH'].isin(df_unique['MaMH'])]
             list_recommend_subjects[''] = np.arange(1, len(list_recommend_subjects) + 1) 
-            list_recommend_subjects = list_recommend_subjects.reindex(columns=['', 'MaMH', 'Course_Name', 'Prof_Name', 'Duration', 'Elective', 'Size_Course'])
+            list_recommend_subjects = list_recommend_subjects.reindex(columns=['', 'MaMH', 'Course_Name', 'Prof_Name', 'Duration', 'Group_Lab', 'Size_Course'])
             with st.expander("List of recommend subjects in this semester"):    
                 st.dataframe(list_recommend_subjects.set_index(''))  
 
