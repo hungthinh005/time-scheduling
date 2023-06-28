a95dcdef29593c09beb2db82139a25f94d5c360a
Phan Hưng Thịnh <69240815+hungthinh005@users.noreply.github.com>
2023-06-28 14:33:21 +0700

    Update processing.py

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -251,6 +251,6 @@ if __name__ == "__main__":
         if st.button('Get Filter'):
             if 'html_result' in locals():
                 filtered_html = get_filter(html_result, list_filter)
-                st.markdown(filtered_html, unsafe_allow_html=True)
+                st.write(filtered_html, unsafe_allow_html=True)
         # if 'filtered_html' in locals():
         #     st.markdown(filtered_html, unsafe_allow_html=True)
