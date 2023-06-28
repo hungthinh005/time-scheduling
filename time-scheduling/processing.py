5908234429837991d9c6efc120428051243c7bf4
hungthinh005 <phanhungthinh555@gmail.com>
2023-06-29 00:04:00 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -254,8 +254,6 @@ if __name__ == "__main__":
         filter = df_room['Room'].to_list()
         list_filter = st.sidebar.multiselect('Room Filter', filter, filter)
         list_filter1 = [item for sublist in list_filter for item in sublist]        
-        list_filter1 = ' '.join(list_filter1)
-        list_filter1 = ast.literal_eval(list_filter1)
         st.write(list_filter1)
         if st.sidebar.button('Get Filter'):
             if list_filter:
