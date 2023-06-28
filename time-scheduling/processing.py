49ddd0309f86b91231b81b585271be5588c69637
hungthinh005 <phanhungthinh555@gmail.com>
2023-06-28 23:42:42 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -158,6 +158,7 @@ def get_filter(html_result, list_filter):
     div_elements = soup.find_all('div', id=lambda x: x and x.startswith('room_'))
     # Filter and display the schedule for specific rooms
     filtered = ""
+    st.write("func", list_filter)
     for div in div_elements:
         room_id = div['id'].replace('room_', '')  # Extract the room ID from the div's id attribute
         if room_id in list_filter:
