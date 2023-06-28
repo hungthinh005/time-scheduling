f4b6170efa139c9634ac1cd4ca6f99fcccf6e402
hungthinh005 <phanhungthinh555@gmail.com>
2023-06-29 00:22:58 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -256,9 +256,9 @@ if __name__ == "__main__":
         filter = df_room['Room'].to_list()
         list_filter = st.sidebar.multiselect('Room Filter', filter, filter)
         if st.sidebar.button('Get Filter'):
-            if list_filter:
-                filtered = get_filter(session_state['html_result'], list_filter)
-                st.markdown(BeautifulSoup(filtered, 'html.parser'), unsafe_allow_html=True)
+            # if list_filter:
+            filtered = get_filter(session_state['html_result'], list_filter)
+            st.markdown(BeautifulSoup(filtered, 'html.parser'), unsafe_allow_html=True)
 
 
         # except:
