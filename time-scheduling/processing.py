f4fca60cb3e5274741c15385bd69e9f858afb016
Phan Hưng Thịnh <69240815+hungthinh005@users.noreply.github.com>
2023-06-28 13:59:08 +0700

    Update processing.py

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -158,7 +158,7 @@ def get_filter(html_result, list_filter):
     for div in div_elements:
         room_id = div['id'].replace('room_', '')  # Extract the room ID from the div's id attribute
         if room_id in list_filter:
-            filtered_html += str(div)
+            filtered_html.append(div)
     # st.markdown(filtered_html, unsafe_allow_html=True)
     st.write(filtered_html)
     return filtered_html
