cb97941698c90b40f3d93f85602bc11869ff82d9
Phan Hưng Thịnh <69240815+hungthinh005@users.noreply.github.com>
2023-06-28 10:43:02 +0700

    Update ConsoleApp.py

--- time-scheduling/ConsoleApp.py
+++ time-scheduling/ConsoleApp.py
@@ -28,11 +28,11 @@ def main(file_name, list_filter):
     # alg = Hgasso(configuration)
     alg.run()
     html_result = HtmlOutput.getResult(alg.result)
-
+    return html_result
     # temp_file_path = tempfile.gettempdir() + file_name.replace(".json", ".html")
     # writer = codecs.open(temp_file_path, "w", "utf-8")
 
-
+def filter(html_result):
     # Parse the HTML
     soup = BeautifulSoup(html_result, 'html.parser')
     # Find all div elements with id starting with 'room_'
