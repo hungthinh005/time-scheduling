48b92087fa4a5483e97aaa74394c0d703ece5a6a
Phan Hưng Thịnh <69240815+hungthinh005@users.noreply.github.com>
2023-06-29 10:19:34 +0700

    Update processing.py

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
         if st.button('Generate'): 
             session_state['html_result'] = main(file_name)
             # st.write(session_state['html_result'])
-            st.markdown(session_state['html_result'], unsafe_allow_html=True)
+        st.markdown(session_state['html_result'], unsafe_allow_html=True)
 
 
         filter = df_room['Room'].to_list()
