6f512e81d6f62d98039b194bbdaa964250a95733
hungthinh005 <phanhungthinh555@gmail.com>
2023-06-29 22:40:16 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -253,12 +253,12 @@ if __name__ == "__main__":
 
             if st.button('Generate'): 
                 # session_state['html_result'] = main(file_name)
-                html_result = main(file_name)
+                session_state['html_result'] = main(file_name)
 
         except:
             traceback.print_exc()
         list_filter = st.sidebar.multiselect('Room Filter', filter, filter)
-        filtered1 = get_filter(html_result, list_filter)
+        filtered1 = get_filter(session_state['html_result'], list_filter)
         st.markdown(filtered1, unsafe_allow_html=True)  
     with tab2:         
         for_stu()
