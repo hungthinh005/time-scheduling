e345e48e6d43d973c077749b87064d1eda5d81b8
hungthinh005 <phanhungthinh555@gmail.com>
2023-06-30 00:49:56 +0700

    z

--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 
 
 
-def load_file():
+def load_file(df2, df_room):
     
     list_course = []
     index_count_course_id = 0
@@ -169,81 +169,77 @@ def get_filter(html, list_filter):
             filtered += str(div)
     return filtered
 
+def data_display():
+
+    uploaded_file = st.file_uploader('')
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+
+    else:
+        df = [['Data Mining', 1, 35, 4, 'Nguyen Thi Thanh Sang', 'IT132IU'],
+            ['Analytics for Observational Data', 2, 35, 4, 'Nguyen Thi Thanh Sang', 'IT142IU'],
+            ['Fundamentals of Programming', 0, 90, 3, 'Dao Tran Hoang Chau', 'IT149IU'],
+            ['Object-Oriented Analysis and Design', 0, 90, 4, 'Ha Viet Uyen Synh', 'IT090IU']]
+        room_columns = ['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']
+        df = pd.DataFrame(df, columns=room_columns)
+
+    df1 = df[['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']]
+    df1 = df1.rename(columns={'TenMH': 'Course_Name', 'ToTH': 'Group_Lab', 'TongSoSV': 'Size_Course', 'SoTiet': 'Duration', 'TenDayDuNV': 'Prof_Name', 'MaMH': 'MaMH'})
+    df1['Lab'] = df1['Group_Lab']
+    # df1['Lab'] = df1['Lab'].astype(str)
+    for index, row in df1.iterrows():
+        if row['Lab'] == 1 or row['Lab'] == 2 or row['Lab'] == 3 or row['Lab'] == 4:
+            df1.at[index, 'Lab'] = 'True'
+        else:
+            df1.at[index, 'Lab'] = ''
+    df1['Lab'] = df1['Lab'].astype(bool)
+    
+
+    ## create default room
+    room_default = [['A1.309', 90, 0],
+                    ['L107', 90, 0],
+                    ['LA1.605', 60, 1],
+                    ['La1.607', 60, 1]
+    ]
+    room_columns = ['Room', 'Size_Room', 'Lab']
+    df_room = pd.DataFrame(room_default, columns=room_columns)
+    df_room['Lab'] = df_room['Lab'].astype(str)
+    for index, row in df_room.iterrows():
+        if row['Lab'] == '1':
+            df_room.at[index, 'Lab'] = 'True'
+        else:
+            df_room.at[index, 'Lab'] = ''
+    df_room['Lab'] = df_room['Lab'].astype(bool)
+
+
+    col1, col2, col3, col4 = st.columns([0.5,7,2.4,0.5])
+
+    with col2:
+        df2 = st.experimental_data_editor(df1, num_rows="dynamic")
+        df2['Size_Course'] = df2['Size_Course'].astype(int)
+        df2['Duration'] = df2['Duration'].astype(int)
+        df2['Group_id'] = np.arange(1, len(df2) + 1)
+
+    with col3:                          
+        df_room = st.experimental_data_editor(df_room, num_rows="dynamic")
+        df_room['Size_Room'] = df_room['Size_Room'].astype(int)
+        filter = df_room['Room'].to_list()
+        # df_room_filter = df_room[df_room['Room'].isin(list_filter)]   
+
+    return df2, df_room, filter
+    # with col3:
+    #     with st.expander("Descriptions for Data Input"):    
+    #         st.write("- Must Include: Course Name, Lab Group, Size of Course, Duration (Period of Course), Professor Name.")
+    #         st.write("- In the case of the course with 4 periods, 1 room can only accommodate 12 classes at most. Be careful when modifying the info on rooms")
 
 
 st.set_page_config(layout="wide")
 if __name__ == "__main__":
     st.markdown("<h1 style='text-align: center; color: white;'>Time Scheduling Engine</h1>", unsafe_allow_html=True)
 
-    session_state = {}
-    session_state1 = {}
-
-
     tab1, tab2, tab3 = st.tabs(["Schedule", "Student", "Filter"])
     with tab1:
-
-        uploaded_file = st.file_uploader('')
-        if uploaded_file is not None:
-            df = pd.read_csv(uploaded_file)
- 
-        else:
-            df = [['Data Mining', 1, 35, 4, 'Nguyen Thi Thanh Sang', 'IT132IU'],
-                ['Analytics for Observational Data', 2, 35, 4, 'Nguyen Thi Thanh Sang', 'IT142IU'],
-                ['Fundamentals of Programming', 0, 90, 3, 'Dao Tran Hoang Chau', 'IT149IU'],
-                ['Object-Oriented Analysis and Design', 0, 90, 4, 'Ha Viet Uyen Synh', 'IT090IU']]
-            room_columns = ['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']
-            df = pd.DataFrame(df, columns=room_columns)
-
-        df1 = df[['TenMH', 'ToTH', 'TongSoSV', 'SoTiet', 'TenDayDuNV', 'MaMH']]
-        df1 = df1.rename(columns={'TenMH': 'Course_Name', 'ToTH': 'Group_Lab', 'TongSoSV': 'Size_Course', 'SoTiet': 'Duration', 'TenDayDuNV': 'Prof_Name', 'MaMH': 'MaMH'})
-        df1['Lab'] = df1['Group_Lab']
-        # df1['Lab'] = df1['Lab'].astype(str)
-        for index, row in df1.iterrows():
-            if row['Lab'] == 1 or row['Lab'] == 2 or row['Lab'] == 3 or row['Lab'] == 4:
-                df1.at[index, 'Lab'] = 'True'
-            else:
-                df1.at[index, 'Lab'] = ''
-        df1['Lab'] = df1['Lab'].astype(bool)
-        
-
-        ## create default room
-        room_default = [['A1.309', 90, 0],
-                        ['L107', 90, 0],
-                        ['LA1.605', 60, 1],
-                        ['La1.607', 60, 1]
-        ]
-        room_columns = ['Room', 'Size_Room', 'Lab']
-        df_room = pd.DataFrame(room_default, columns=room_columns)
-        df_room['Lab'] = df_room['Lab'].astype(str)
-        for index, row in df_room.iterrows():
-            if row['Lab'] == '1':
-                df_room.at[index, 'Lab'] = 'True'
-            else:
-                df_room.at[index, 'Lab'] = ''
-        df_room['Lab'] = df_room['Lab'].astype(bool)
-
-
-        col1, col2, col3, col4 = st.columns([0.5,7,2.4,0.5])
-
-        with col2:
-            df2 = st.experimental_data_editor(df1, num_rows="dynamic")
-            df2['Size_Course'] = df2['Size_Course'].astype(int)
-            df2['Duration'] = df2['Duration'].astype(int)
-            df2['Group_id'] = np.arange(1, len(df2) + 1)
-
-        with col3:                          
-            df_room = st.experimental_data_editor(df_room, num_rows="dynamic")
-            df_room['Size_Room'] = df_room['Size_Room'].astype(int)
-            filter = df_room['Room'].to_list()
-            # df_room_filter = df_room[df_room['Room'].isin(list_filter)]   
-
-      
-        # with col3:
-        #     with st.expander("Descriptions for Data Input"):    
-        #         st.write("- Must Include: Course Name, Lab Group, Size of Course, Duration (Period of Course), Professor Name.")
-        #         st.write("- In the case of the course with 4 periods, 1 room can only accommodate 12 classes at most. Be careful when modifying the info on rooms")
-    
-        
+        df2, df_room, filter = data_display()
         load_file()
         file_name = "/GaSchedule1.json"
         html_result = main(file_name)
