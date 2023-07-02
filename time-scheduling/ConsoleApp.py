16ada6d73698c4a1fc6ab64917a84704e4a68cb2
hungthinh005 <phanhungthinh555@gmail.com>
2023-07-02 16:14:57 +0700

    z

--- time-scheduling/ConsoleApp.py
+++ time-scheduling/ConsoleApp.py
@@ -35,14 +35,14 @@ def main(file_name):
 @st.cache_data()
 def main_filter(file_name):
     # start_time = int(round(time.time() * 1000))
-    configuration_filter = Configuration()
-    target_file_filter = str(pathlib.Path().absolute()) + file_name
-    configuration_filter.parseFile(target_file_filter)
-    alg_filter = NsgaII(configuration_filter)
+    configuration = Configuration()
+    target_file = str(pathlib.Path().absolute()) + file_name
+    configuration.parseFile(target_file)
+    alg = NsgaII(configuration)
     # alg = Hgasso(configuration)
     
-    alg_filter.run()
-    html_result_filter = HtmlOutput.getResult(alg_filter.result)
+    alg.run()
+    html_result_filter = HtmlOutput.getResult(alg.result)
     # st.markdown(html_result, unsafe_allow_html=True)
     # seconds = (int(round(time.time() * 1000)) - start_time) / 1000.0
     # st.write("\nCompleted in {} secs.\n".format(seconds))
