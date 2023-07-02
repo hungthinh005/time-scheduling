00edce764fe640ef1c11014f56c38d6482b87f39
hungthinh005 <phanhungthinh555@gmail.com>
2023-07-02 15:14:28 +0700

    z

--- time-scheduling/ConsoleApp.py
+++ time-scheduling/ConsoleApp.py
@@ -17,6 +17,20 @@ import streamlit as st
 # from algorithm.Hgasso import Hgasso
 from HtmlOutput import HtmlOutput
 
+def main(file_name):
+    start_time = int(round(time.time() * 1000))
+    configuration = Configuration()
+    target_file = str(pathlib.Path().absolute()) + file_name
+    configuration.parseFile(target_file)
+    alg = NsgaII(configuration)
+    # alg = Hgasso(configuration)
+    
+    alg.run()
+    html_result = HtmlOutput.getResult(alg.result)
+    # st.markdown(html_result, unsafe_allow_html=True)
+    seconds = (int(round(time.time() * 1000)) - start_time) / 1000.0
+    st.write("\nCompleted in {} secs.\n".format(seconds))
+    return html_result
 
 @st.cache_data()
 def main_filter(file_name):
@@ -34,20 +48,7 @@ def main_filter(file_name):
     # st.write("\nCompleted in {} secs.\n".format(seconds))
     return html_result_filter
 
-def main(file_name):
-    start_time = int(round(time.time() * 1000))
-    configuration = Configuration()
-    target_file = str(pathlib.Path().absolute()) + file_name
-    configuration.parseFile(target_file)
-    alg = NsgaII(configuration)
-    # alg = Hgasso(configuration)
-    
-    alg.run()
-    html_result = HtmlOutput.getResult(alg.result)
-    # st.markdown(html_result, unsafe_allow_html=True)
-    seconds = (int(round(time.time() * 1000)) - start_time) / 1000.0
-    st.write("\nCompleted in {} secs.\n".format(seconds))
-    return html_result
+
 
     # temp_file_path = tempfile.gettempdir() + file_name.replace(".json", ".html")
     # writer = codecs.open(temp_file_path, "w", "utf-8")
--- time-scheduling/processing.py
+++ time-scheduling/processing.py
@@ -248,12 +248,13 @@ if __name__ == "__main__":
         html_result = main(file_name)
         html_result_filter = main_filter(file_name)
         list_filter = st.sidebar.multiselect('Room Filter', filter, filter)
+        if st.button('Save'):
+            html_result_filter = html_result
         if st.button('Generate'):
             st.markdown(html_result, unsafe_allow_html=True)
         if st.sidebar.button('Get Filter'): 
             filtered1 = get_filter(html_result_filter, list_filter)
             st.markdown(filtered1, unsafe_allow_html=True)
-
 
         # if len(sys.argv) > 1:
         #     file_name = sys.argv[1]
