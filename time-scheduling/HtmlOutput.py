8b387cd7e0c9abed3e66ed412c5e8fe3ea7eae2a
Phan Hưng Thịnh <69240815+hungthinh005@users.noreply.github.com>
2023-07-04 17:23:15 +0700

    Update HtmlOutput.py

--- time-scheduling/HtmlOutput.py
+++ time-scheduling/HtmlOutput.py
@@ -25,15 +25,14 @@ class HtmlOutput:
         # CRITERIAS_DESCR = HtmlOutput.CRITERIAS_DESCR
 
         # sb = ["MH: ", cc.Course.Name, "<br />GV: ", cc.Professor.Name, "<br />Room: ", "/".join(map(lambda grp: grp.Name, cc.Groups)),"<br />"]
-        getRoomById = configuration.getRoomById
-        room = getRoomById(roomId)
+
         sb = []
         sb.append(" <span style='color:#00FFFF' title=''> <b>MH: <b/> </span>")
         sb.append(cc.Course.Name)
         sb.append("<br /> <span style='color:#00FFFF' title=''> <b>GV: <b/> </span>")
         sb.append(cc.Professor.Name)
         # sb.append("<br /> <span style='color:#00FFFF' title=''> <b>Room: <b/> </span>")
-        sb.append(room.Name)
+        # sb.append(room.Name)
         # sb.append("/".join(map(lambda grp: grp.Name, cc.Groups)),)
         
         if cc.LabRequired:
