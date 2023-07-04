db91ec08baacee12321065661bfedf55e52e87ac
hungthinh005 <phanhungthinh555@gmail.com>
2023-07-05 00:29:28 +0700

    z

--- time-scheduling/HtmlOutput.py
+++ time-scheduling/HtmlOutput.py
@@ -157,7 +157,7 @@ class HtmlOutput:
                 if periodId == HtmlOutput.ROOM_ROW_NUMBER - 1:
                     sb.append("</table>\n</div>\n")
             
-            sb.replace(" <b>Room: <b/> </span>", "<b> <b>Room: <b/> </span>{}".format(room.Name))
+            # sb.replace(" <b>Room: <b/> </span>", "<b> <b>Room: <b/> </span>{}".format(room.Name))
             st.markdown(sb)
         return "".join(str(v) for v in sb)
     
