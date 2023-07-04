e66ffa54f12d550301629a8ac512ea8f16b5ac30
hungthinh005 <phanhungthinh555@gmail.com>
2023-07-04 23:01:48 +0700

    z

--- time-scheduling/HtmlOutput.py
+++ time-scheduling/HtmlOutput.py
@@ -135,7 +135,6 @@ class HtmlOutput:
 
         slot_table = defaultdict(list)
         time_table = HtmlOutput.generateTimeTable(solution, slot_table)  # Tuple[0] = time, Tuple[1] = roomId
-        st.markdown(time_table)
 
         if not slot_table or not time_table:
             return ""
@@ -171,6 +170,7 @@ class HtmlOutput:
 
                 if periodId == HtmlOutput.ROOM_ROW_NUMBER - 1:
                     sb.append("</table>\n</div>\n")
+            st.markdown(sb)
         return "".join(str(v) for v in sb)
     
     
