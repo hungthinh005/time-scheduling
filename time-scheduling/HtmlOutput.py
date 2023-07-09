9860d3bae57a5ba8944dcb9bee44137f50101b08
hungthinh005 <phanhungthinh555@gmail.com>
2023-07-09 15:13:56 +0700

    z

--- time-scheduling/HtmlOutput.py
+++ time-scheduling/HtmlOutput.py
@@ -27,7 +27,7 @@ class HtmlOutput:
         sb.append(cc.Course.Name)
         sb.append("<br /> <span style='color:#00FFFF' title=''> <b>GV: <b/> </span>")
         sb.append(cc.Professor.Name)
-        # sb.append("<br /> <span style='color:#00FFFF' title=''> <b>Room: <b/> </span>")
+        sb.append("<br /> <span style='color:#00FFFF' title=''> </span>")
         # sb.append(room.Name)
         # sb.append("/".join(map(lambda grp: grp.Name, cc.Groups)),)
         
